f88b09a0773327330034945a6d2c4f5b6e77348e
jerry_s <gitter0759@gmail.com>
2015-01-12 23:20:31 +0800

    Fix Demo compile error.

--- KDCycleBannerViewDemo/Pods/SDWebImage/SDWebImage/SDWebImageDownloaderOperation.m
+++ KDCycleBannerViewDemo/Pods/SDWebImage/SDWebImage/SDWebImageDownloaderOperation.m
@@ -35,6 +35,8 @@
     UIImageOrientation orientation;
     BOOL responseFromCached;
 }
+@synthesize executing = _executing;
+@synthesize finished = _finished;
 
 - (id)initWithRequest:(NSURLRequest *)request options:(SDWebImageDownloaderOptions)options progress:(void (^)(NSInteger, NSInteger))progressBlock completed:(void (^)(UIImage *, NSData *, NSError *, BOOL))completedBlock cancelled:(void (^)())cancelBlock {
     if ((self = [super init])) {
